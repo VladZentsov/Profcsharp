9b3115304b53b0847474732974228d19bcb01a9f
Sergey Kamyanetskiy <i@ksergey.ru>
2021-11-21 11:33:12 +0300

    fix. update log->logger

---
+++ ProfcsharpShow/S1E1Calculator/CalculatorCore/Logger/ILogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorCore.Log
+{
+    public interface ILog
+    {
+        void Log(string ingo);
+    }
+
+    public class FileLog : ILog
+    {
+        public void Log(string info)
+        {
+            File.AppendAllText("log.db", $"{info}{Environment.NewLine}");
+        }
+    }
+
+    public class DebugLog : ILog
+    {
+        public void Log(string info)
+        {
+            Debug.WriteLine($"{info}{Environment.NewLine}");
+        }
+    }
+}
